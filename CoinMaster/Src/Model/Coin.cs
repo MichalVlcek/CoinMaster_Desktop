bb173c7db587929370d3719be92a2463ce88f680
Michal Vlcek <xvlcek3@fi.muni.cz>
2021-06-02 22:06:50 +0200

    Added model class for Coin

---
+++ CoinMaster/Src/Model/Coin.cs
@@ -0,0 +1,24 @@
+﻿namespace CoinMaster.Src.Model
+{
+    public class Coin
+    {
+        public string Icon { get; }
+        public int Rank { get; }
+        public string Name { get; }
+        public string Symbol { get; }
+        public decimal Price { get; }
+        public decimal MarketCap { get; }
+        public decimal Supply { get; }
+
+        public Coin(string icon, int rank, string name, string symbol, decimal price, decimal marketCap, decimal supply)
+        {
+            Icon = icon;
+            Rank = rank;
+            Name = name;
+            Symbol = symbol;
+            Price = price;
+            MarketCap = marketCap;
+            Supply = supply;
+        }
+    }
+}
