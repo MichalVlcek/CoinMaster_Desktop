ff97d3e8352369bea0c409b0d7a10c7b43602aa9
Michal Vlcek <xvlcek3@fi.muni.cz>
2021-06-02 23:33:29 +0200

    implemented view switching

--- CoinMaster/Src/ViewModel/MainViewModel.cs
+++ CoinMaster/Src/ViewModel/MainViewModel.cs
@@ -5,8 +5,10 @@ namespace CoinMaster.Src.ViewModel
     public class MainViewModel : ObservableObject
     {
         public RelayCommand HomeScreenCommand { get; set; }
+        public RelayCommand AddCoinCommand { get; set; }
         
         public HomeScreenViewModel HomeScreenViewModel { get; set; }
+        public AddCoinViewModel AddCoinViewModel { get; set; }
         
         private object _currentView;
         public object CurrentView
@@ -22,9 +24,11 @@ namespace CoinMaster.Src.ViewModel
         public MainViewModel()
         {
             HomeScreenViewModel = new HomeScreenViewModel();
+            AddCoinViewModel = new AddCoinViewModel();
             CurrentView = HomeScreenViewModel;
 
             HomeScreenCommand = new RelayCommand(_ => CurrentView = HomeScreenViewModel);
+            AddCoinCommand = new RelayCommand(_ => CurrentView = AddCoinViewModel);
         }
     }
 }
