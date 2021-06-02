d272a322ba0841306935b7dd91117f4f5356f712
Michal Vlcek <xvlcek3@fi.muni.cz>
2021-06-02 20:19:53 +0200

    changed structure of files

---
+++ CoinMaster/Src/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CoinMaster.Src.Core;
+
+namespace CoinMaster.Src.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeScreenCommand { get; set; }
+        
+        public HomeScreenViewModel HomeScreenViewModel { get; set; }
+        
+        private object _currentView;
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeScreenViewModel = new HomeScreenViewModel();
+            CurrentView = HomeScreenViewModel;
+
+            HomeScreenCommand = new RelayCommand(_ => CurrentView = HomeScreenViewModel);
+        }
+    }
+}
