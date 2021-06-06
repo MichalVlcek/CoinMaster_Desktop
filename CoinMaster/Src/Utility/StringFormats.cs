1c351538f1e20a2f8661e09c87edeb7dad95e7bd
Michal Vlcek <xvlcek3@fi.muni.cz>
2021-06-06 12:13:20 +0200

    fixed formatting

--- CoinMaster/Src/Utility/StringFormats.cs
+++ CoinMaster/Src/Utility/StringFormats.cs
@@ -8,12 +8,12 @@ namespace CoinMaster.Utility
         private const string Culture = "en-US";
 
         public static string RankFormat(int rank) => $"#{rank}";
-        public static string PercentFormat(double percent) => $"{percent:0.##)}%";
+        public static string PercentFormat(double percent) => $"{percent:0.##}%";
 
         public static string CurrencyFormat(decimal price)
             => price.ToString("C", CultureInfo.CreateSpecificCulture(Culture));
         
         public static string CurrencyFormat(decimal price, string symbol)
-            => price.ToString("N", CultureInfo.CreateSpecificCulture(Culture)) + symbol.ToUpper();
+            => price.ToString("N", CultureInfo.CreateSpecificCulture(Culture)) + " " + symbol.ToUpper();
     }
 }
