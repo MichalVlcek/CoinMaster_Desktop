6ca1437b5ffa8b0ca9e9894c9078d987611b2a31
Michal Vlcek <xvlcek3@fi.muni.cz>
2021-06-07 19:49:18 +0200

    moved formating properties to Coin class

--- CoinMaster/Src/Model/Coin.cs
+++ CoinMaster/Src/Model/Coin.cs
@@ -1,34 +1,76 @@
 ﻿using System.Text.Json.Serialization;
+using CoinMaster.Utility;
 
 namespace CoinMaster.Model
 {
     public class Coin
     {
-        [JsonPropertyName("image")] public string Icon { get; set; }
-        [JsonPropertyName("market_cap_rank")] public int Rank { get; set; }
-        [JsonPropertyName("name")] public string Name { get; set; }
-        [JsonPropertyName("symbol")] public string Symbol { get; set; }
-        [JsonPropertyName("current_price")] public decimal Price { get; set; }
-        [JsonPropertyName("market_cap")] public decimal MarketCap { get; set; }
+        [JsonPropertyName("image")] public string Icon { get; }
+        [JsonPropertyName("market_cap_rank")] public int Rank { get; }
+        [JsonPropertyName("name")] public string Name { get; }
+        [JsonPropertyName("symbol")] public string Symbol { get; }
+        [JsonPropertyName("current_price")] public decimal Price { get; }
+        [JsonPropertyName("market_cap")] public decimal MarketCap { get; }
 
         [JsonPropertyName("circulating_supply")]
-        public decimal CirculatingSupply { get; set; }
+        public decimal CirculatingSupply { get; }
 
-        [JsonPropertyName("max_supply")] public decimal? MaxSupply { get; set; }
-        [JsonPropertyName("ath")] public decimal Ath { get; set; }
+        [JsonPropertyName("max_supply")] public decimal? MaxSupply { get; }
+        [JsonPropertyName("ath")] public decimal Ath { get; }
 
         [JsonPropertyName("ath_change_percentage")]
-        public double AthPercentChange { get; set; }
+        public double AthPercentChange { get; }
 
-        [JsonPropertyName("atl")] public decimal Atl { get; set; }
+        [JsonPropertyName("atl")] public decimal Atl { get; }
 
         [JsonPropertyName("atl_change_percentage")]
-        public double AtlPercentChange { get; set; }
-        
+        public double AtlPercentChange { get; }
+
         [JsonPropertyName("price_change_percentage_24h_in_currency")]
-        public double? PriceChangePercent24H { get; set; }
-        
+        public double? PriceChangePercent24H { get; }
+
         [JsonPropertyName("price_change_percentage_7d_in_currency")]
-        public double? PriceChangePercent7D { get; set; }
+        public double? PriceChangePercent7D { get; }
+
+        public Coin(string icon, int rank, string name, string symbol, decimal price, decimal marketCap,
+            decimal circulatingSupply, decimal? maxSupply, decimal ath, double athPercentChange, decimal atl,
+            double atlPercentChange, double? priceChangePercent24H, double? priceChangePercent7D)
+        {
+            Icon = icon;
+            Rank = rank;
+            Name = name;
+            Symbol = symbol;
+            Price = price;
+            MarketCap = marketCap;
+            CirculatingSupply = circulatingSupply;
+            MaxSupply = maxSupply;
+            Ath = ath;
+            AthPercentChange = athPercentChange;
+            Atl = atl;
+            AtlPercentChange = atlPercentChange;
+            PriceChangePercent24H = priceChangePercent24H;
+            PriceChangePercent7D = priceChangePercent7D;
+        }
+
+        public string Title => $"{Name} - {Symbol.ToUpper()}";
+        public string RankFormat => StringFormats.RankFormat(Rank);
+        public string PriceFormat => StringFormats.CurrencyFormat(Price);
+        public string AthFormat => StringFormats.CurrencyFormat(Ath, Symbol);
+        public string AtlFormat => StringFormats.CurrencyFormat(Atl, Symbol);
+        public string AthChangeFormat => StringFormats.PercentFormat(AthPercentChange);
+        public string AtlChangeFormat => StringFormats.PercentFormat(AtlPercentChange);
+        public string MarketCapFormat => StringFormats.CurrencyFormat(MarketCap, Symbol);
+
+        public string CirculatingSupplyFormat => StringFormats.CurrencyFormat(CirculatingSupply, Symbol);
+
+        public string MaximumSupplyFormat => StringFormats.CurrencyFormat(MaxSupply ?? 0, Symbol);
+        public string PriceChangePercent24HFormat => StringFormats.PercentFormat(PriceChangePercent24H ?? 0);
+        public string PriceChangePercent7DFormat => StringFormats.PercentFormat(PriceChangePercent7D ?? 0);
+
+        public string PriceChange24HFormat =>
+            StringFormats.CurrencyFormat(CoinUtils.CalculatePriceChange(Price, PriceChangePercent24H ?? 0));
+
+        public string PriceChange7DFormat =>
+            StringFormats.CurrencyFormat(CoinUtils.CalculatePriceChange(Price, PriceChangePercent7D ?? 0));
     }
 }
--- CoinMaster/Src/ViewModel/AddCoinPanelViewModel.cs
+++ CoinMaster/Src/ViewModel/AddCoinPanelViewModel.cs
@@ -14,49 +14,8 @@ namespace CoinMaster.ViewModel
         public Coin SelectedCoin
         {
             get => _selectedCoin;
-            private set
-            {
-                SetAndNotify(ref _selectedCoin, value); 
-                OnPropertyChanged(nameof(Title));
-                OnPropertyChanged(nameof(Rank));
-                OnPropertyChanged(nameof(Price));
-                OnPropertyChanged(nameof(Ath));
-                OnPropertyChanged(nameof(Atl));
-                OnPropertyChanged(nameof(AthChange));
-                OnPropertyChanged(nameof(AtlChange));
-                OnPropertyChanged(nameof(MarketCap));
-                OnPropertyChanged(nameof(CirculatingSupply));
-                OnPropertyChanged(nameof(MaximumSupply));
-                OnPropertyChanged(nameof(PriceChangePercent24H));
-                OnPropertyChanged(nameof(PriceChangePercent7D));
-                OnPropertyChanged(nameof(PriceChange24H));
-                OnPropertyChanged(nameof(PriceChange7D));
-            }
+            private set => SetAndNotify(ref _selectedCoin, value);
         }
-
-        public string Title => $"{SelectedCoin.Name} - {SelectedCoin.Symbol.ToUpper()}";
-        public string Rank => StringFormats.RankFormat(SelectedCoin.Rank);
-        public string Price => StringFormats.CurrencyFormat(SelectedCoin.Price);
-        public string Ath => StringFormats.CurrencyFormat(SelectedCoin.Ath, SelectedCoin.Symbol);
-        public string Atl => StringFormats.CurrencyFormat(SelectedCoin.Atl, SelectedCoin.Symbol);
-        public string AthChange => StringFormats.PercentFormat(SelectedCoin.AthPercentChange);
-        public string AtlChange => StringFormats.PercentFormat(SelectedCoin.AtlPercentChange);
-        public string MarketCap => StringFormats.CurrencyFormat(SelectedCoin.MarketCap, SelectedCoin.Symbol);
-
-        public string CirculatingSupply =>
-            StringFormats.CurrencyFormat(SelectedCoin.CirculatingSupply, SelectedCoin.Symbol);
-
-        public string MaximumSupply => StringFormats.CurrencyFormat(SelectedCoin.MaxSupply ?? 0, SelectedCoin.Symbol);
-        public string PriceChangePercent24H => StringFormats.PercentFormat(SelectedCoin.PriceChangePercent24H ?? 0);
-        public string PriceChangePercent7D => StringFormats.PercentFormat(SelectedCoin.PriceChangePercent7D ?? 0);
-
-        public string PriceChange24H =>
-            StringFormats.CurrencyFormat(CoinUtils.CalculatePriceChange(SelectedCoin.Price,
-                SelectedCoin.PriceChangePercent24H ?? 0));
-
-        public string PriceChange7D =>
-            StringFormats.CurrencyFormat(CoinUtils.CalculatePriceChange(SelectedCoin.Price,
-                SelectedCoin.PriceChangePercent7D ?? 0));
 
         public AddCoinPanelViewModel(IEventAggregator eventAggregator)
         {
