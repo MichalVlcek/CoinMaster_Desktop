2f591675fc87b93f1a600169a57b78a5dd388e2d
Michal Vlcek <xvlcek3@fi.muni.cz>
2021-06-08 13:44:48 +0200

    implemented showing of basic info of selected coin

--- CoinMaster/Src/ViewModel/CoinOverviewViewModel.cs
+++ CoinMaster/Src/ViewModel/CoinOverviewViewModel.cs
@@ -1,10 +1,26 @@
-﻿using CoinMaster.Interfaces;
+﻿using CoinMaster.Events;
+using CoinMaster.Model;
 using Stylet;
 
 namespace CoinMaster.ViewModel
 {
-    public class CoinOverviewViewModel : Screen
+    public class CoinOverviewViewModel : Screen, IHandle<CoinSelectedEvent>
     {
+        private Coin _selectedCoin;
+        public Coin SelectedCoin
+        {
+            get => _selectedCoin;
+            private set => SetAndNotify(ref _selectedCoin, value);
+        }
 
+        public CoinOverviewViewModel(IEventAggregator eventAggregator)
+        {
+            eventAggregator.Subscribe(this);
+        }
+        
+        public void Handle(CoinSelectedEvent message)
+        {
+            SelectedCoin = message.Coin;
+        }
     }
 }
--- CoinMaster/Src/ViewModel/HomeScreenViewModel.cs
+++ CoinMaster/Src/ViewModel/HomeScreenViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using CoinMaster.Events;
 using CoinMaster.Interfaces;
 using CoinMaster.Model;
 using Stylet;
@@ -9,14 +10,27 @@ namespace CoinMaster.ViewModel
     {
         public List<Coin> Coins => TmpDatabase.Coins;
 
+        private Coin _selectedCoin;
+        public Coin SelectedCoin
+        {
+            get => _selectedCoin;
+            set
+            {
+                SetAndNotify(ref _selectedCoin, value);
+                events.Publish(new CoinSelectedEvent {Coin = SelectedCoin});
+            }
+        }
+
         public DashboardOverviewViewModel DashboardOverview { get; }
 
         private readonly INavigationController navigationController;
+        private readonly IEventAggregator events;
 
-        public HomeScreenViewModel(INavigationController navigationController,
+        public HomeScreenViewModel(INavigationController navigationController, IEventAggregator events,
             DashboardOverviewViewModel dashboardOverviewViewModel)
         {
             this.navigationController = navigationController;
+            this.events = events;
             DashboardOverview = dashboardOverviewViewModel;
         }
 
