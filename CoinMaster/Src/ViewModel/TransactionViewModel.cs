89d269c2367253bd25d07754dc6e55ad609d864c
Michal Vlcek <xvlcek3@fi.muni.cz>
2021-06-16 10:03:19 +0200

    added functions for buttons in transaction view

--- CoinMaster/Src/ViewModel/TransactionViewModel.cs
+++ CoinMaster/Src/ViewModel/TransactionViewModel.cs
@@ -11,9 +11,8 @@ namespace CoinMaster.ViewModel
         private IEventAggregator eventAggregator;
 
         public TransactionEditViewModel TransactionEdit { get; }
-        
-        private BindingList<Transaction> _transactions;
 
+        private BindingList<Transaction> _transactions;
         public BindingList<Transaction> Transactions
         {
             get => _transactions;
@@ -21,7 +20,6 @@ namespace CoinMaster.ViewModel
         }
 
         private Transaction _selectedTransaction;
-
         public Transaction SelectedTransaction
         {
             get => _selectedTransaction;
@@ -32,7 +30,8 @@ namespace CoinMaster.ViewModel
             }
         }
 
-        public TransactionViewModel(IEventAggregator eventAggregator, TransactionEditViewModel transactionEdit) : base(eventAggregator)
+        public TransactionViewModel(IEventAggregator eventAggregator, TransactionEditViewModel transactionEdit)
+            : base(eventAggregator)
         {
             this.eventAggregator = eventAggregator;
             TransactionEdit = transactionEdit;
@@ -48,8 +47,22 @@ namespace CoinMaster.ViewModel
                     CoinPrice = 52000,
                     Fee = 2,
                     Description = "ajaoa"
+                },
+                new Transaction
+                {
+                    Type = TransactionType.BUY,
+                    Date = DateTime.Now,
+                    CoinId = "bitcoin",
+                    Amount = 0.125m,
+                    CoinPrice = 52000,
+                    Fee = 2,
+                    Description = "ajaoa"
                 }
             };
         }
+
+        public void AddNewTransaction() => SelectedTransaction = Transaction.EmptyTransaction;
+
+        public void DeleteTransactions() => Transactions.Remove(SelectedTransaction);
     }
 }
