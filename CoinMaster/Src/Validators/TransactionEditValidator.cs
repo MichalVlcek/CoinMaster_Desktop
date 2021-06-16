ffd46ae73655da0d68bf912e05e457a5d7290bf7
Michal Vlcek <xvlcek3@fi.muni.cz>
2021-06-16 17:00:09 +0200

    added validators

---
+++ CoinMaster/Src/Validators/TransactionEditValidator.cs
@@ -0,0 +1,23 @@
+﻿using CoinMaster.ViewModel;
+using FluentValidation;
+
+namespace CoinMaster.Validators
+{
+    public class TransactionEditValidator : AbstractValidator<TransactionEditViewModel>
+    {
+        private const string NumberRegex = "^[0-9][0-9.]*$";
+
+        public TransactionEditValidator()
+        {
+            RuleFor(x => x.CoinPriceText)
+                .NotEmpty().WithMessage("{PropertyName} can't be empty")
+                .Matches(NumberRegex).WithMessage("{PropertyName} must be number");
+            RuleFor(x => x.AmountText)
+                .NotEmpty().WithMessage("{PropertyName} can't be empty")
+                .Matches(NumberRegex).WithMessage("{PropertyName} must be number");
+            RuleFor(x => x.FeeText)
+                .NotEmpty().WithMessage("{PropertyName} can't be empty")
+                .Matches(NumberRegex).WithMessage("{PropertyName} must be number");
+        }
+    }
+}
