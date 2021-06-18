58526e23208aa6912ba5b8f4aaac7c0c9445f77d
Michal Vlcek <xvlcek3@fi.muni.cz>
2021-06-18 22:33:52 +0200

    added new view

---
+++ CoinMaster/Src/ViewModel/MainContentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CoinMaster.Interfaces;
+using Stylet;
+
+namespace CoinMaster.ViewModel
+{
+    public class MainContentViewModel : Conductor<IScreen>.StackNavigation, INavigationControllerDelegate
+    {
+        public MenuBarViewModel MenuBar { get; }
+
+        public MainContentViewModel(MenuBarViewModel menuBar)
+        {
+            MenuBar = menuBar;
+        }
+        
+        public void NavigateTo(IScreen screen)
+        {
+            ActivateItem(screen);
+        }
+    }
+}
