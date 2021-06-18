6eff17c17dc19e01150199fd522aa04695f44b8f
Michal Vlcek <xvlcek3@fi.muni.cz>
2021-06-18 22:43:41 +0200

    register ui created and navigation implemented

--- CoinMaster/Src/ViewModel/RegisterViewModel.cs
+++ CoinMaster/Src/ViewModel/RegisterViewModel.cs
@@ -1,9 +1,19 @@
-﻿using Stylet;
+﻿using CoinMaster.Interfaces;
+using Stylet;
 
 namespace CoinMaster.ViewModel
 {
     public class RegisterViewModel : Screen
     {
+        private readonly INavigationControllerAuthentication navigationController;
+
+        public RegisterViewModel(
+            INavigationControllerAuthentication navigationController)
+        {
+            this.navigationController = navigationController;
+        }
         
+        public void NavigateToMain() => navigationController.NavigateToMain();
+        public void NavigateToLogin() => navigationController.NavigateToLogin();
     }
 }
